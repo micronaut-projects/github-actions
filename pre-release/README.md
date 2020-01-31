c20c6a6189545f6cc74e02583ed39a37fac40c57
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-01-31 12:47:34 +0100

    pre-release action

---
+++ pre-release/README.md
@@ -0,0 +1,11 @@
+# Micronaut pre-release action
+
+Performs some actions before doing a release
+
+## Example usage
+
+```yaml
+uses: alvarosanchez/github-actions/pre-release@master
+with:
+  token: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ pre-release/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -l
+
+#!/bin/bash
+# $1 == GH_TOKEN
+
+echo -n "Determining release version: "
+release_version=${GITHUB_REF:11}
+echo $release_version
+
+echo "Configuring git"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global user.name "${GITHUB_ACTOR}"
+git fetch
+
+echo -n "Determining target branch: "
+target_branch=`cat $GITHUB_EVENT_PATH | jq '.release.target_commitish' | sed -e 's/^"\(.*\)"$/\1/g'`
+echo $target_branch
+git checkout $target_branch
+
+echo "Setting release version in gradle.properties"
+sed -i "s/^projectVersion.*$/projectVersion\=${release_version}/" gradle.properties
+cat gradle.properties
+
+echo "Pushing release version and recreating v${release_version} tag"
+git add gradle.properties
+git commit -m "Release v${release_version}"
+git push origin $target_branch
+git push origin :refs/tags/v${release_version}
+git tag -fa v${release_version} -m "Release v${release_version}"
+git push origin $target_branch --tags
+
+echo "Closing again the release after updating the tag"
+release_url=`cat $GITHUB_EVENT_PATH | jq '.release.url' | sed -e 's/^"\(.*\)"$/\1/g'`
+echo $release_url
+curl -s --request PATCH -H "Authorization: Bearer $1" -H "Content-Type: application/json" $release_url --data "{\"draft\": false}"
