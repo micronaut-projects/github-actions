f63de6d1637fc5149dda2bf62e292fe6591cdf17
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-02-18 00:34:40 +0100

    Fix variables

--- release-notes/entrypoint.sh
+++ release-notes/entrypoint.sh
@@ -6,8 +6,11 @@
 organisation=`dirname $2`
 repository=`basename $2`
 
-echo -n "Determining last version: "
+echo -n "Determining lastest tag: "
 latest_tag=`curl -s "https://api.github.com/repos/$2/releases/latest" | jq '.tag_name' | sed -e 's/"//g'`
+echo $latest_tag
+
+echo -n "Determining lastest version: "
 latest_version=`echo $latest_tag | sed -e 's/v//g'`
 echo $latest_version
 
@@ -24,4 +27,4 @@ github_changelog_generator --user $organisation --project $repository --token $1
         --removed-labels "type: removed" \
         --usernames-as-github-logins \
         --release-branch "$3" \
-        --since-tag "$latest_tag"
+        --since-tag $latest_tag
