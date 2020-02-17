b8374f2a87ccf95a55c6227eddd3730216b6ccab
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-02-18 00:22:36 +0100

    Better release notes generation

--- release-notes/entrypoint.sh
+++ release-notes/entrypoint.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 # $1 == GH_TOKEN
 # $2 == repository (eg: Codertocat/Hello-World)
+# $3 == target branch (eg: master)
 
 organisation=`dirname $2`
 repository=`basename $2`
 
-github_changelog_generator --user $organisation --project $repository --token $1 --header-label "" --enhancement-labels "type: enhancement" --bug-labels "type: bug" --exclude-labels "status: stale,closed: notabug,closed: duplicate,closed: question,closed: invalid,closed: won't fix" --breaking-labels "type: breaking" --deprecated-labels "type: deprecated" --removed-labels "type: removed" --usernames-as-github-logins --unreleased-only --release-branch "master"
-#--release-branch "1.3.x" --since-tag "v1.3.9"
+echo -n "Determining last version: "
+latest_tag=`curl -s "https://api.github.com/repos/$2/releases/latest" | jq '.tag_name'`
+latest_version=`echo $latest_tag | sed -e 's/[v,"]//g'`
+echo $latest_version
+
+echo -n "Determining next version: "
+next_version=`/increment_version.sh -p $release_version`
+echo $next_version
+
+github_changelog_generator --user $organisation --project $repository --token $1 \
+        --enhancement-labels "type: enhancement" \
+        --bug-labels "type: bug" \
+        --exclude-labels "status: stale,closed: notabug,closed: duplicate,closed: question,closed: invalid,closed: won't fix" \
+        --breaking-labels "type: breaking" \
+        --deprecated-labels "type: deprecated" \
+        --removed-labels "type: removed" \
+        --usernames-as-github-logins \
+        --release-branch "$3" \
+        --since-tag "$latest_tag"
