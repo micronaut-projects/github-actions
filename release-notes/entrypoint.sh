52653cf5b0925b2e7a1e9dc4fe1f079f0f04c240
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-02-17 23:50:59 +0100

    No header label

--- release-notes/entrypoint.sh
+++ release-notes/entrypoint.sh
@@ -5,5 +5,5 @@
 organisation=`dirname $2`
 repository=`basename $2`
 
-github_changelog_generator --user $organisation --project $repository --token $1  --enhancement-labels "type: enhancement" --bug-labels "type: bug" --exclude-labels "status: stale,closed: notabug,closed: duplicate,closed: question,closed: invalid,closed: won't fix" --breaking-labels "type: breaking" --deprecated-labels "type: deprecated" --removed-labels "type: removed" --usernames-as-github-logins --unreleased-only
+github_changelog_generator --user $organisation --project $repository --token $1 --header-label "" --enhancement-labels "type: enhancement" --bug-labels "type: bug" --exclude-labels "status: stale,closed: notabug,closed: duplicate,closed: question,closed: invalid,closed: won't fix" --breaking-labels "type: breaking" --deprecated-labels "type: deprecated" --removed-labels "type: removed" --usernames-as-github-logins --unreleased-only --release-branch "master"
 #--release-branch "1.3.x" --since-tag "v1.3.9"
