518b25665b9ea6db09622a3502f978bb13357f33
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-04-29 10:52:10 +0200

    Fail on error

--- update-bom/entrypoint.sh
+++ update-bom/entrypoint.sh
@@ -4,6 +4,8 @@
 # $3 == property
 # $4 == version
 
+set -e
+
 echo "Configuring git"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git config --global user.name "${GITHUB_ACTOR}"
