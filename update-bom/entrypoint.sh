a41838bdfdd9ee8c1e127afe1a4e1718107eea77
graemerocher <graeme.rocher@gmail.com>
2020-05-11 11:11:26 +0200

    Restore remainder of action

--- update-bom/entrypoint.sh
+++ update-bom/entrypoint.sh
@@ -11,7 +11,7 @@ git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git config --global user.name "${GITHUB_ACTOR}"
 cd micronaut-core
 projectName="${GITHUB_REPOSTIROY:19}"
-# git checkout -b "$projectName-$4"
+git checkout -b "$projectName-$4"
 trimSpace="$(echo -e "$3" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 IFS=$'\n'       
 for j in $trimSpace
@@ -21,11 +21,11 @@ do
     sed -i "s/^$propertyName.*$/$propertyName\=${propertyVersion}/" gradle.properties
 done
 
-echo "Diffing"
+echo "Changes Applied"
 git diff
 
-# echo "Creating pull request"
-# git add gradle.properties
-# git commit -m "Bump $projectName to $4"
-# git push origin "$projectName-$4"
-# curl -s --request POST -H "Authorization: Bearer $1" -H "Content-Type: application/json" https://api.github.com/repos/micronaut-projects/micronaut-core/pulls --data "{\"title\": \"Bump $projectName to $4\", \"head\":\"$projectName-$4\", \"base\":\"$2\"}"
+echo "Creating pull request"
+git add gradle.properties
+git commit -m "Bump $projectName to $4"
+git push origin "$projectName-$4"
+curl -s --request POST -H "Authorization: Bearer $1" -H "Content-Type: application/json" https://api.github.com/repos/micronaut-projects/micronaut-core/pulls --data "{\"title\": \"Bump $projectName to $4\", \"head\":\"$projectName-$4\", \"base\":\"$2\"}"
