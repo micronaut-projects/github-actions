900792c73f8d5cef97a369820edfd36153c5ac61
graemerocher <graeme.rocher@gmail.com>
2020-05-11 10:12:58 +0200

    test properties

--- update-bom/entrypoint.sh
+++ update-bom/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # $1 == GH_TOKEN
 # $2 == branch
-# $3 == property
+# $3 == properties
 # $4 == version
 
 set -e
@@ -12,12 +12,14 @@ git config --global user.name "${GITHUB_ACTOR}"
 cd micronaut-core
 git checkout -b "$3-$4"
 
-echo "Setting release version in gradle.properties"
-sed -i "s/^$3.*$/$3\=${4}/" gradle.properties
-cat gradle.properties
+echo "What is properties? $3"
 
-echo "Creating pull request"
-git add gradle.properties
-git commit -m "Bump $3 to $4"
-git push origin "$3-$4"
-curl -s --request POST -H "Authorization: Bearer $1" -H "Content-Type: application/json" https://api.github.com/repos/micronaut-projects/micronaut-core/pulls --data "{\"title\": \"Bump $3 to $4\", \"head\":\"$3-$4\", \"base\":\"$2\"}"
+# echo "Setting release version in gradle.properties"
+# sed -i "s/^$3.*$/$3\=${4}/" gradle.properties
+# cat gradle.properties
+
+# echo "Creating pull request"
+# git add gradle.properties
+# git commit -m "Bump $3 to $4"
+# git push origin "$3-$4"
+# curl -s --request POST -H "Authorization: Bearer $1" -H "Content-Type: application/json" https://api.github.com/repos/micronaut-projects/micronaut-core/pulls --data "{\"title\": \"Bump $3 to $4\", \"head\":\"$3-$4\", \"base\":\"$2\"}"
