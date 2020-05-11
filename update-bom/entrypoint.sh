069ce88a88d988a2c14644d309eec944cdb44564
graemerocher <graeme.rocher@gmail.com>
2020-05-11 11:09:36 +0200

    Output diff

--- update-bom/entrypoint.sh
+++ update-bom/entrypoint.sh
@@ -10,23 +10,22 @@ echo "Configuring git"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git config --global user.name "${GITHUB_ACTOR}"
 cd micronaut-core
-# git checkout -b "$3-${GITHUB_REPOSTIROY:19}"
-
+projectName="${GITHUB_REPOSTIROY:19}"
+# git checkout -b "$projectName-$4"
 trimSpace="$(echo -e "$3" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 IFS=$'\n'       
 for j in $trimSpace
 do
-    propertyName=$(echo "$j" | awk -F "@" '{print $1}')
-    propertyVersion=$(echo "$j" | awk -F "@" '{print $2}')
+    propertyName=$(echo "$j" | awk -F "=" '{print $1}')
+    propertyVersion=$(echo "$j" | awk -F "=" '{print $2}')
     sed -i "s/^$propertyName.*$/$propertyName\=${propertyVersion}/" gradle.properties
 done
 
-# echo "Setting release version in gradle.properties"
-# sed -i "s/^$3.*$/$3\=${4}/" gradle.properties
-cat gradle.properties
+echo "Diffing"
+git diff
 
 # echo "Creating pull request"
 # git add gradle.properties
-# git commit -m "Bump $3 to $4"
-# git push origin "$3-$4"
-# curl -s --request POST -H "Authorization: Bearer $1" -H "Content-Type: application/json" https://api.github.com/repos/micronaut-projects/micronaut-core/pulls --data "{\"title\": \"Bump $3 to $4\", \"head\":\"$3-$4\", \"base\":\"$2\"}"
+# git commit -m "Bump $projectName to $4"
+# git push origin "$projectName-$4"
+# curl -s --request POST -H "Authorization: Bearer $1" -H "Content-Type: application/json" https://api.github.com/repos/micronaut-projects/micronaut-core/pulls --data "{\"title\": \"Bump $projectName to $4\", \"head\":\"$projectName-$4\", \"base\":\"$2\"}"
