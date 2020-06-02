6e9292f9bae3f936619ebf2c07a0a8f58f9802a0
Iván López <lopezi@objectcomputing.com>
2020-06-02 10:45:17 +0200

    Calculate latest version per branch

--- release-notes/entrypoint.sh
+++ release-notes/entrypoint.sh
@@ -7,15 +7,27 @@ organisation=`dirname $2`
 repository=`basename $2`
 
 echo -n "Determining lastest tag: "
-latest_tag=`curl -s "https://api.github.com/repos/$2/releases/latest" | jq '.tag_name' | sed -e 's/"//g'`
+latest_tag=`curl -s https://api.github.com/repos/$2/releases | jq -cr ".[] | select (.target_commitish == \"$3\") | .tag_name" | head -1`
+
 echo $latest_tag
 
 echo -n "Determining lastest version: "
 latest_version=`echo $latest_tag | sed -e 's/v//g'`
 echo $latest_version
 
+if [ -z $latest_version ]
+then
+  if [ "$3" == "master" ]
+  then
+    next_version="1.0.0"
+  else
+    next_version=`echo $3 | sed -e 's/x/0/g'`
+  fi
+else
+  next_version=`./increment_version.sh -p $latest_version`
+fi
+
 echo -n "Determining next version: "
-next_version=`/increment_version.sh -p $latest_version`
 echo $next_version
 
 echo "::set-output name=next_version::$next_version"
